1c80451df842629dd0a028cc88d4541e6cf319fc
unknown <tongnguyennhattien140805@gmail.com>
2026-06-16 10:27:00 +0700

    complete create user

--- src/components/layouts/Sidebar.tsx
+++ src/components/layouts/Sidebar.tsx
@@ -1,12 +1,16 @@
+"use client"
 import { ActiveLink } from "@/common";
 import { menuItems } from "@/constants";
 import { TMenuItems } from "@/types";
 import { UserButton } from "@clerk/nextjs";
 import Link from "next/link";
 import { ModeToggle } from "../ui/darkmode";
+import { useAuth } from "@clerk/nextjs";
+import { IconUser } from "../icons";
 
 
 const Sidebar = () => {
+    const { userId } = useAuth();
     return (
         <div className="p-5 border-r border-r-gray-200 dark:border-grayDark/50 dark:bg-grayDarker flex flex-col h-screen">
             <Link href="/" className="font-bold text-3xl inline-block mb-5"><span className="text-primary">U</span>CADEMY</Link>
@@ -17,7 +21,9 @@ const Sidebar = () => {
             </ul>
             <div className="mt-auto flex items-center justify-end gap-5">
                 <ModeToggle></ModeToggle>
-                <UserButton></UserButton>
+                {!userId ? (
+                    <Link href={"/sign-in"} className="size-10  p-1 flex items-center justify-center rounded-lg bg-primary hover:bg-primary/80 text-white dark:text-grayDark px-2 py-1.5 transition-all"><IconUser /></Link>
+                ) : (<UserButton></UserButton>)}
             </div>
 
         </div>
