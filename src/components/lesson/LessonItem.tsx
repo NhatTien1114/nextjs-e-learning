c45dafe71808a4c4f56c9ce980cb9a4aebdebf72
unknown <tongnguyennhattien140805@gmail.com>
2026-07-02 23:55:39 +0700

    video 42

--- src/components/lesson/LessonItem.tsx
+++ src/components/lesson/LessonItem.tsx
@@ -7,7 +7,7 @@ const LessonItem = ({ lesson, url }: { lesson: { title: string, duration: number
         <div className="flex items-center gap-2 bgDarkMode rounded-lg p-4 text-base font-medium mb-5">
             <IconPlay />
             {url ? <Link href={"/"}>{lesson.title}</Link> : <h4>{lesson.title}</h4>}
-            <span className="ml-auto text-xs text-grayDark">{lesson.duration} phút</span>
+            <span className="ml-auto text-xs text-grayDark">{lesson.duration} phút hai</span>
         </div>
     )
 }
